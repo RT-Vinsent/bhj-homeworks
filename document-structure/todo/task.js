65772ac687dc3ed3a47e14377f7e8f4a79224168
Roman T <rt.hagbo@gmail.com>
2021-12-19 02:11:26 +0500

    дз №6, Задача №2 Простой список дел, со *(упрощение кода, хотя как сказать)

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,63 +4,59 @@
 let taskInput = document.getElementById('task__input'); // поле ввода
 let tasksAdd = document.getElementById('tasks__add'); // кнопка добавить задачу
 let tasksList = document.getElementById('tasks__list'); // список задач
-let keyCount = 0; // счётчик заметок
-let myStorage = {}; // объект моего хранилища
+let localStorageNotes = JSON.parse(localStorage.getItem("notes")); // парсим notes из localStorage
 
-// спарсим myStorage из localStorage обратно в объект
-let returnMyStorage = JSON.parse(localStorage.getItem("myStorageKey"));
-
-if (returnMyStorage) { // если в localStorage есть моё хранилище  
-    myStorage = returnMyStorage; // то Моё хранилище равняется хранилищу в localStorage
-    keyCount = localStorage.getItem('keyCount'); // переназнаает keyCount из keyCount в localStorage
-}
-
-for (let key in returnMyStorage) { // обход по объекту 
-    tasksList.insertAdjacentHTML('afterBegin', returnMyStorage[key]); // вставляем заметку из хранилища в DOM
-    taskRemoteFunc(); // вызываем функцию которая назначает удаления заметок при клике на крестик
-}
+if (localStorageNotes != null) { // если в localStorage есть notes  
+    for (let i = 0; i < localStorageNotes.length; i++) { // то цикл по массиву
+        htmlNoteAdd(localStorageNotes[i]); // добавляем заметки в DOM из notes
+        taskRemoteFunc(); // вызываем функцию которая назначает удаления заметок при клике на крестик
+    }
+};
 
 function taskRemoteFunc() { // функция назначает удаление заметок при клике на крестик
     let taskRemove = tasksList.querySelectorAll('.task__remove'); // элементы крестика
-
+    
+    // taskRemove[0], потому что новая заметка всегда с индексом 0 
     taskRemove[0].addEventListener('click', function(evt) { // клик по крестику
         evt.preventDefault(); // отмена стандартного действия
         
-        // taskRemove[0], потому что новая заметка всегда с индексом 0
         let task = taskRemove[0].closest('.task'); // родитель крестика
-        let dataKey = task.dataset.key; // dataKey заметки
+        let taskText = task.querySelector('.task__title').innerText; // текс заметки
+
+        updateLocalStorage(null, taskText); // обновляем localStorage
 
         task.remove(); // удаление заметки
-        delete myStorage[dataKey]; // удаления заметки из хранилища по dataKey
-
-        localStorage.setItem('myStorageKey', JSON.stringify(myStorage)); // обновляем моё хранилище в localStorage
     })
 }
 
 tasksAdd.addEventListener('click', function(evt) { // клик по кнопке
     evt.preventDefault(); // отмена стандартного действия
 
-    // присвоение значения инпута и keyCount в разметку, и всё это в переменную
-    let html = `
-    <div class="task" data-key="${keyCount}">
-        <div class="task__title">
-            ${taskInput.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-    </div>`
-
     if (taskInput.value) { // если инпут не пустой
-        tasksList.insertAdjacentHTML('afterBegin', html); // добавляем заметку в начало
-
-        myStorage[keyCount] = html; // добавляем заметку в моё хранилище
-
-        localStorage.setItem('myStorageKey', JSON.stringify(myStorage)); // обновляем моё хранилище в localStorage
-
-        taskInput.value = ''; // очищаем поле ввода
-
-        keyCount++; // увеличиваем счётчик заметок
-        localStorage.setItem('keyCount', keyCount); // записываем счётчик в localStorage
-
+        htmlNoteAdd(taskInput.value); // добавляем html разметку в DOM
+        updateLocalStorage(taskInput.value, null); // обновляем localStorage
         taskRemoteFunc(); // вызываем функцию которая назначает удаления заметок при клике на крестик
+        taskInput.value = ''; // очищаем поле ввода
     }
 });
+
+function updateLocalStorage(noteAdd, noteDel) { // добавляем заметку в localStorage
+    let notes = JSON.parse(localStorage.getItem("notes")); // парсим notes из localStorage
+
+    if (notes != null && noteAdd != null) {notes.push(noteAdd)};  // пушит массив
+    if (notes === null && noteAdd != null) {notes = [noteAdd]};  // создаёт массив
+    if (notes != null && noteDel != null) {notes.splice(notes.indexOf(noteDel), 1)};  // удаляет заметку из массива
+
+    localStorage.setItem('notes', JSON.stringify(notes)); // перезаписываем notes в localStorage
+}
+
+function htmlNoteAdd(note) { // добавляем html разметку в DOM
+    let html = `
+    <div class="task">
+        <div class="task__title">
+            ${note}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>`;
+    tasksList.insertAdjacentHTML('afterBegin', html); // добавляем заметку в начало
+}
